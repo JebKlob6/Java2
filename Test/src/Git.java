b4580a5313f9f78f30d2f0bbc8b3846b889d3780
Jeb Klobocishta <jebklob01@gmail.com>
2023-11-19 00:10:54 -0500

    Added a count system from 1-5

--- Test/src/Git.java
+++ Test/src/Git.java
@@ -11,6 +11,14 @@ public class Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub\
 	System.out.println("Hello World");
-	}
+	
+	  
+	  for (int i = 1; i <= 5; i++) {
 
+          // Press Ctrl+D to start debugging your code. We have set one breakpoint
+          // for you, but you can always add more by pressing Cmd+F8.
+          System.out.println("i = " + i);
+      }
+
+	}
 }
